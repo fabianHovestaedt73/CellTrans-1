676eff5972ee7855504cfc8864f99fe8895cc84f
fabianHovestaedt73 <hovestaedtfabian99@web.de>
2024-01-11 14:29:01 +0100

    edit userinterface_file

--- R/shiny_ui.R
+++ R/shiny_ui.R
@@ -14,9 +14,10 @@ shiny_ui <- fluidPage(
       uiOutput("cellTypes"),
       numericInput("timenr", "Number of time points", value = 5),
       fileInput("inputFile", "Select a text file, where the time points are stored"),
-      textInput("inputFolder", "Enter the path (folder), where the distribution matrices are stored"),
+      textInput("inputFolder", "Enter the path (folder), where the transition matrices are stored"),
       checkboxInput("identityMatrix",
                     label = "Identity matrix (pure initial cell compositions)", value = TRUE),
+      numericInput("tauSlider", "Value of tau", value = 1),
       selectInput("dropdown", "Select a cellstate for a more detailed investigation:", choices=1),
       actionButton("loadDataBtn", "Calculate")
     ),
